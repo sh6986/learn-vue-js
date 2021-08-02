d854a137241cf046511e71fd46c85b847af46fa4
okok960411 <okok960411@gmail.com>
2021-08-03 00:40:51 +0900

    싱글 파일 컴포넌트 소개 및 여태까지 배운 내용 적용하는 방법

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,27 @@
+var appHeader = {
+    template: '<div>hearder</div>',
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+
+<template>
+  <div>hearder</div>
+</template>
+
+<script>
+export default {
+    // javascript - 인스턴스 옵션
+    methods: {
+        addNum: function() {
+            
+        }
+    }
+}
+</script>
+
+<style>
+    /* CSS */
+</style>
