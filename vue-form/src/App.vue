146a109940b2790542c28f706d25d3447ce5ec2e
seunghyun_oh <okok960411@gmail.com>
2021-08-07 19:39:12 +0900

    프로젝트 생성 및 마크업 작업

---
+++ vue-form/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <form action="">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text">
+    </div>
+    <div>
+      <label for="password"></label>
+      <input id="password" type="password">
+    </div>
+    <button>login</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
