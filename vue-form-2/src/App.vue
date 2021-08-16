4db661dc17d527917240e1c4afd9ab70d5aa058f
okok960411 <okok960411@gmail.com>
2021-08-17 00:05:57 +0900

    v-model 속성과 submit 이벤트 처리

---
+++ vue-form-2/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <form v-on:submit="submitForm">
+      <div>
+        <label for="username">id: </label>
+        <input type="text" id="username" v-model="username">
+      </div>
+      <div>
+        <label for="password">pw:</label>
+        <input type="password" id="password" v-model="password">
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function(event) {
+      event.preventDefault();
+      console.log(this.username, this.password);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
